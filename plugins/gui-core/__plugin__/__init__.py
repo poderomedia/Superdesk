21eece80a42fd726b7ab7759ea0493314b7273b0
Gabriel Nistor <gabriel.nistor@sourcefabric.org>
2012-03-07 13:01:26 +0200

    NR-14 : Internationalization
    Created the scanner and new internationalization support based on GNU gettext.
    Also refactored some sql alchemy components.

--- plugins/gui-core/__plugin__/__init__.py
+++ plugins/gui-core/__plugin__/__init__.py
@@ -1,9 +1,9 @@
 '''
 Created on Jul 15, 2011
 
-@package ally core request
-@copyright 2011 Sourcefabric o.p.s.
-@license http://www.gnu.org/licenses/gpl-3.0.txt
+@package: GUI core
+@copyright: 2011 Sourcefabric o.p.s.
+@license: http://www.gnu.org/licenses/gpl-3.0.txt
 @author: Mihai Balaceanu
 
 Special package that is targeted by the IoC for processing plugins.
